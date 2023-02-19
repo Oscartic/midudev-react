696bf436024a39f7dba7359a4f153e7ea8c016a6
Oscar Ballesteros <oscartic@gmail.com>
2023-02-19 17:32:58 -0300

    add class cursor

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 
 const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
+    // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el componente
+
+  // pointer move
   useEffect(() => {
     console.log('effect', { enabled })
-
+  
     const handleMove = (event) => {
       const { clientX, clientY } = event
       setPosition({ x: clientX, y: clientY })
@@ -25,21 +30,33 @@ const FollowMouse = () => {
       window.removeEventListener('pointermove', handleMove)
     }
   }, [enabled])
+
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
   return (
     <>
       <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}></div>
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+      }}>
+
+      </div>
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
