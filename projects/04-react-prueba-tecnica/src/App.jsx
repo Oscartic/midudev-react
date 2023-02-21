582bf3f8239442565522899d155a855e5b039dd1
Oscar Ballesteros <oscartic@gmail.com>
2023-02-21 14:53:08 -0300

    separate fetch in two useEffect

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import './App.css'
+
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`; 
@@ -8,28 +10,30 @@ export function App () {
   const [fact, setFact] = useState('Lorem ipsum cat');
   const [imageUrl, setImageUrl] = useState();
   
+  // fetch the cat quote when reload page
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
         const { fact } = data;
         setFact(fact);
-        const threeFirstWord = fact.split(' ', 3).join(' ');
-        console.log(threeFirstWord);
-
-        fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            console.log(response)
-            const { url } = response;
-            setImageUrl(url);
-          })
       }) 
-
-
   }, [])
 
-   
+  // recover image when we have quote
+  useEffect(() => {
+    if(!fact) return;
+    const threeFirstWord = fact.split(' ', 3).join(' ');
+    console.log(threeFirstWord);
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        const { url } = response;
+        setImageUrl(url);
+      })
+  }, [fact])
 
   return (
     <main>
