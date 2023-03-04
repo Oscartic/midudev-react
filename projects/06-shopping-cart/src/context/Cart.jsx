17e1da7f12734b2e28d77a7c4c47f85a9a4baee5
Oscar Ballesteros <oscartic@gmail.com>
2023-03-04 17:08:34 -0300

    end project cart

--- projects/06-shopping-cart/src/context/Cart.jsx
+++ projects/06-shopping-cart/src/context/Cart.jsx
@@ -1,32 +1,42 @@
-import { createContext, useState } from 'react';
+import { useReducer, createContext } from 'react';
+import { cartInitialState, cartReducer } from '../reducers/CartReducer';
+
 // ~ 1. Create context
 export const CartContext = createContext()
+
+// * this is a reducer hook
+function useCartReducer() {
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState)
+
+  const addToCart = (product) => dispatch({
+    type: 'ADD_TO_CART',
+    payload: product
+  })
+
+  const removeFromCart = (product) => dispatch({
+    type: 'REMOVE_FROM_CART',
+    payload: product
+  })
+
+  const clearCart = () => dispatch({
+    type: 'CLEAR_CART'
+  })
+
+  return { state, addToCart, removeFromCart, clearCart }
+}
+
 // ~ 2. Create provider
+// the react context dependency is minuscule when isolate from the rest in the custom hooks
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([])
-
-  const addToCart = (product) => {
-    const productInCartIndex = cart.findIndex(item => item.id === product.id)
-    if (productInCartIndex >= 0) {
-      // * deep clone of the cart with structuredClone
-      const newCart = structuredClone(cart)
-      newCart[productInCartIndex].quantity += 1
-      return setCart(newCart)
-    }
-    // ? product is not in the cart
-    setCart(prevState => [...prevState, { ...product, quantity: 1 }])
-  }
-
-  const removeFromCart = (product) => {
-    setCart(prevState => prevState.filter(item => item.id !== product.id))
-  }
-
-  const clearCart = () => {
-    setCart([])
-  }
-
+  const { state, addToCart, removeFromCart, clearCart } = useCartReducer()
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+    <CartContext.Provider value={{
+      cart: state,
+      addToCart,
+      removeFromCart,
+      clearCart
+    }}
+    >
       {children}
     </CartContext.Provider>
   )
